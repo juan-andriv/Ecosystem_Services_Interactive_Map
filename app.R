18df368c0cfe544e916d131eee362ab112b21eb1
Juan Andrade Rivera <163057641+juan-andriv@users.noreply.github.com>
2024-03-28 13:56:39 +0100

    Add files via upload

--- app.R
+++ app.R
@@ -12,6 +12,7 @@ library(shiny)
 library(leaflet)
 library(dplyr)
 library(leaflet.extras)
+library(ggplot2)
 
 # Import data ----
 data <- read.csv("Data/Ecosystem_Services_latlong.csv", header = TRUE)
@@ -20,8 +21,8 @@ data <- read.csv("Data/Ecosystem_Services_latlong.csv", header = TRUE)
 ui <- fluidPage(
   titlePanel("Ecosystem services provided per tree"), # application title
   mainPanel(
-    leafletOutput(outputId = "map"), # space to display the map
-    absolutePanel(top = 0, right = 15, width = 200,
+    leafletOutput(outputId = "map", height = 600), # space to display the map
+    absolutePanel(top = 0, right = 15, width = 200, # Add a panel to select services
                   style = "background-color: white; border: 1px solid #999999;", # make the panel white with gray border
                   selectInput("services", "Select ecosystem services",
                             choices = c("Avoided runoff" = "Avoided.Runoff..m3.year.",
@@ -29,15 +30,19 @@ ui <- fluidPage(
                                         "Total pollution removed" = "Total.Pollution.Removed..g.year.",
                                         "VOC production" = "VOC.Produced..g.year.",
                                         "Water interception" = "Water.Intercepted..m3.year."),
-                            selected = NULL,
-               )
-             )
-          )  
-     ) 
+                            selected = NULL),
+               ),
+             
+      absolutePanel(bottom = 25, right = 15, width = 200, height = "80%",
+                    style = "background-color: white; border: 1px solid #999999;",
+                    plotOutput("graph", height = "100%")
+      )  
+     )
+)
 
 # server.R ----
 server <- function(input, output) {
-  
+
 # Create the leaflet map
 output$map <- renderLeaflet({
 
@@ -62,6 +67,9 @@ categories <- cut(filtered_data[[input$services]],
 # create color palette
 pal <- colorBin(palette = c("palegreen", "lightseagreen", "mediumseagreen", "firebrick"), domain = c(0, max_value), bins = 10, na.color = rgb(0, 0, 0, alpha = 0))
 
+# save them as reactiveValues object
+colors <- reactiveValues(palette = pal(filtered_data[[input$services]]))
+
 # create the leaflet map
 map <- leaflet(data = filtered_data) %>%
       setView(lng = -101.015261, lat = 22.144573, zoom = 16) %>%
@@ -82,17 +90,33 @@ map <- leaflet(data = filtered_data) %>%
                          "<strong>Species: </strong>", data$Species, "<br>",
                          "<strong>Height (m): </strong>", data$Height..m., "<br>",
                          "<strong>Cover (m2): </strong>", data$Canopy.cover..m2., "<br>")
-      )
-
-# Create a label
-map %>% addLegend(position = "bottomleft",
-                  pal = pal,
-                  values = categories, 
-                  title = input$services,
-                  labels = as.characters(categories))
+                     ) %>%
+                    # Create a legend
+                    addLegend(position = "bottomleft",
+                       pal = pal,
+                       values = categories, 
+                       title = input$services,
+                       labels = as.characters(categories))
       })
-}
 
+# Create an graph to show distribution of data
+output$graph <- renderPlot({
+  filtered_data <- data %>%
+    filter(!is.na(.data[[input$services]]) & .data[[input$services]] != 0)
+
+# Boxplot specifications  
+  ggplot(filtered_data, aes
+         (x = "", y = .data[[input$services]])) +
+    
+    geom_violin(fill = "skyblue", color = "blue", alpha = 0.6) +
+    geom_boxplot(width = 0.1, fill = "white", color = "black") +
+    labs(title = "Data distribution",
+         x = "Density") +
+    theme_minimal() +
+    theme(plot.title = element_text(hjust = 0.5),
+            axis.ticks.margin = unit(0,'cm'))
+})
+}
 
 # Run the app ----
 shinyApp(ui = ui, server = server)
